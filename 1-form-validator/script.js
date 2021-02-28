44f7787e0859a2b7f247252d20cce5dc31df7dec
jokh0108 <jokh0108@gmail.com>
2021-03-01 06:56:01 +0900

    form-validator: Refactor script

--- 1-form-validator/script.js
+++ 1-form-validator/script.js
@@ -12,12 +12,22 @@ const MIN_USERNAME = 3;
 const MAX_USERNAME = 15;
 const MIN_PASSWORD = 6;
 const MAX_PASSWORD = 25;
+
 const EMAIL_REGEX = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 const LENGTH_STATUS = Object.freeze({
   TOO_SHORT: "short",
   TOO_LONG: "long",
   ENOUGH: "enough",
 });
+
+const CONFIRM_NOT_MATCH_MSG = "Passwords do not match";
+const CONFIRM_REQUIRED_MSG = "Password2 is required";
+
+const NORMAL_CLASSNAME = "normal";
+const SUCCESS_CLASSNAME = "success";
+const ERROR_CLASSNAME = "error";
+const VISIBLE_CLASSNAME = "visible";
+const INVISIBLE_CLASSNAME = "invisible";
 
 const checkLength = (string, min, max) => {
   if (string.length < min) {
@@ -47,17 +57,17 @@ const checkPasswordsMatch = (password1, password2) => {
 };
 
 const showError = (inputElement, errorElement) => {
-  inputElement.classList.remove("normal");
-  inputElement.classList.remove("success");
-  inputElement.classList.add("error");
-  errorElement.classList.replace("invisible", "visible");
+  inputElement.classList.remove(NORMAL_CLASSNAME);
+  inputElement.classList.remove(SUCCESS_CLASSNAME);
+  inputElement.classList.add(ERROR_CLASSNAME);
+  errorElement.classList.replace(INVISIBLE_CLASSNAME, VISIBLE_CLASSNAME);
 };
 
 const showSuccess = (inputElement, errorElement) => {
-  inputElement.classList.remove("normal");
-  inputElement.classList.remove("error");
-  inputElement.classList.add("success");
-  errorElement.classList.replace("visible", "invisible");
+  inputElement.classList.remove(NORMAL_CLASSNAME);
+  inputElement.classList.remove(ERROR_CLASSNAME);
+  inputElement.classList.add(SUCCESS_CLASSNAME);
+  errorElement.classList.replace(VISIBLE_CLASSNAME, INVISIBLE_CLASSNAME);
 };
 
 const onSubmit = (event) => {
@@ -109,11 +119,11 @@ const onSubmit = (event) => {
     if (checkPasswordsMatch(passwordInput.value, confirmInput.value)) {
       showSuccess(confirmInput, confirmError);
     } else {
-      confirmError.innerText = "Passwords do not match";
+      confirmError.innerText = CONFIRM_NOT_MATCH_MSG;
       showError(confirmInput, confirmError);
     }
   } else {
-    confirmError.innerText = "Password2 is required";
+    confirmError.innerText = CONFIRM_REQUIRED_MSG;
     showError(confirmInput, confirmError);
   }
 };
@@ -123,8 +133,3 @@ const init = () => {
 };
 
 init();
-
-// checkRequired() to accept array of inputs
-// checkLength() to check MIN_USERNAME and max length
-// checkEmail() to validate emailInput with regex
-// checkPasswordsMatch() to match confirm password
