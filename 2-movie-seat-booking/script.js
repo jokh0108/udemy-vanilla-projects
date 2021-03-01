1dbf1bc1fa91685df5e2e631b25cb775ed477881
jokh0108 <jokh0108@gmail.com>
2021-03-01 22:54:31 +0900

    movie-seat-booking: Use local storage

--- 2-movie-seat-booking/script.js
+++ 2-movie-seat-booking/script.js
@@ -1,27 +1,75 @@
 const movieSelect = document.querySelector("#jsMovie");
-const seatElements = document.querySelectorAll("#jsSeat");
 const countElement = document.querySelector("#count");
 const totalElement = document.querySelector("#total");
-
-const DEFAULT_PRICE = 10;
+const seatElements = Array.from(document.querySelectorAll("#jsSeat"));
 
 const SELECTED_CLASS = "selected";
 const SELECTED_ATTRIBUTE = "selected";
 const NA_CLASS = "na";
 
-let count = 0;
-let price = DEFAULT_PRICE;
+const PRICE_KEY = "price";
+const SELECTED_SEATS_KEY = "selectedSeats";
 
-const onMovieSelect = (event) => {
-  price = parseInt(event.target.value);
+const DEFAULT_PRICE = 10;
+
+let price = DEFAULT_PRICE;
+let selectedIndexSet = new Set();
+let count = 0;
+
+const savePrice = (price) => {
+  localStorage.setItem(PRICE_KEY, price);
+};
+
+const saveIndexSet = () => {
+  localStorage.setItem(
+    SELECTED_SEATS_KEY,
+    JSON.stringify(Array.from(selectedIndexSet))
+  );
+};
+
+const updateCount = () => {
+  count = selectedIndexSet.size;
+  countElement.innerText = count;
+};
+
+const updateTotal = () => (totalElement.innerText = count * price);
+
+const updateIndexSet = (seatElement, selected) => {
+  const seatIndex = seatElements.indexOf(seatElement);
+  if (selected) {
+    selectedIndexSet.delete(seatIndex);
+  } else {
+    selectedIndexSet.add(seatIndex);
+  }
+  saveIndexSet();
+};
+
+const onClick = (event) => {
+  const clickedSeat = event.target;
+  const selected = clickedSeat.hasAttribute(SELECTED_ATTRIBUTE);
+  toggleSeat(clickedSeat, selected);
+  updateIndexSet(clickedSeat, selected);
+  updateCount();
   updateTotal();
 };
 
-const isSelected = (seatElement) =>
-  seatElement.hasAttribute(SELECTED_ATTRIBUTE);
+const onMovieSelect = (event) => {
+  price = parseInt(event.target.value);
+  savePrice(price);
+  updateTotal();
+};
 
-const toggleSelect = (seatElement) => {
-  if (isSelected(seatElement)) {
+const loadPrice = () => {
+  const savedPrice = localStorage.getItem(PRICE_KEY);
+  if (!savedPrice) return;
+
+  price = savedPrice;
+  const selectedOption = movieSelect.querySelector(`option[value="${price}"]`);
+  selectedOption.setAttribute(SELECTED_ATTRIBUTE, "");
+};
+
+const toggleSeat = (seatElement, selected) => {
+  if (selected) {
     seatElement.removeAttribute(SELECTED_ATTRIBUTE);
     seatElement.classList.replace(SELECTED_CLASS, NA_CLASS);
   } else {
@@ -30,24 +78,25 @@ const toggleSelect = (seatElement) => {
   }
 };
 
-const updateCount = (selected) => {
-  count = selected ? count + 1 : count - 1;
-  countElement.innerText = count;
-};
+const loadSeats = () => {
+  const savedIndexSet = new Set(
+    JSON.parse(localStorage.getItem(SELECTED_SEATS_KEY))
+  );
+  if (!savedIndexSet) return;
 
-const updateTotal = () => {
-  totalElement.innerText = count * price;
-};
-
-const onClick = (event) => {
-  const selectedSeat = event.target;
-  toggleSelect(selectedSeat);
-  const selected = isSelected(selectedSeat);
-  updateCount(selected);
+  seatElements.forEach((seat, index) => {
+    if (savedIndexSet.has(index)) {
+      toggleSeat(seat);
+    }
+  });
+  selectedIndexSet = savedIndexSet;
+  updateCount();
   updateTotal();
 };
 
 const init = () => {
+  loadPrice();
+  loadSeats();
   movieSelect.addEventListener("change", onMovieSelect);
   seatElements.forEach((seat) => {
     seat.addEventListener("click", onClick);
